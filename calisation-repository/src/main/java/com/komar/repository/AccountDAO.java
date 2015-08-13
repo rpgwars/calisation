b09f0bd74aac93700572dd5a5113cde2d3069e95
rpgwars <dagoik@gmail.com>
2015-08-13 13:55:45 +0200

    Functionality account

--- calisation-repository/src/main/java/com/komar/repository/AccountDAO.java
+++ calisation-repository/src/main/java/com/komar/repository/AccountDAO.java
@@ -6,8 +6,6 @@ import com.komar.domain.account.exception.AccountNotFound;
 import com.komar.domain.account.transfer.AccountTO;
 
 public interface AccountDAO extends GenericDAO<Account> {
-
     Account findAccount(String login) throws AccountNotFound;
-
     void saveAccount(AccountTO account) throws AccountAlreadyExists;
 }
